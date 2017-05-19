9961c698009b16545d3be4c08b13edbfc6d2407e
Huy Nguyen <nguyen.dang.huy@framgia.com>
2017-05-19 17:13:55 +0700

    refs #N/A: update ruby version to 2.4.1 in deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ else
   raise "Please set $DEPLOY_REF"
 end
 
-set :rvm_ruby_version, "2.3.1"
+set :rvm_ruby_version, "2.4.1"
 set :deploy_to, "/usr/local/rails_apps/#{fetch :application}"
 case ENV["WEB_SERVER"]
 when "passenger"
