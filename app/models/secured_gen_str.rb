3c0a15ae6c9d43360327888156b66dbe4ec0477c
Huy Nguyen <nguyen.dang.huy@framgia.com>
2017-05-19 10:10:58 +0700

    refs #N/A: add some new stuffs

---
+++ app/models/secured_gen_str.rb
@@ -0,0 +1,15 @@
+module SecuredGenStr
+  private
+  def secured_gen_str attr
+    str = ""
+    loop do
+      str_len = Settings.public_send(self.class.name.underscore.pluralize)
+                .public_send(attr)
+                .public_send(:secure_length).to_i / 2
+      str = SecureRandom.hex str_len
+      break unless self.class.exists?(attr => str)
+    end
+    assign_attribtes attr => str
+    str
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,8 @@ module #{(`echo $REPO_URL`.gsub("\n", "").presence || Dir.pwd).split("/").last.g
     config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
     config.active_record.time_zone_aware_attributes = false
-    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
     config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
 
     Dir.glob("config/routes/*").each do |route|
       config.paths["config/routes.rb"] << Rails.root.join(route)
